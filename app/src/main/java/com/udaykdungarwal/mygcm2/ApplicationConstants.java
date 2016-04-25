792618349cdc5abccabac99ef27d27ecee070ae6
Uday Dungarwal <uday@uday-macbook-pro.local>
2016-04-24 19:48:14 -0700

    Adding Code

---
+++ app/src/main/java/com/udaykdungarwal/mygcm2/ApplicationConstants.java
@@ -0,0 +1,16 @@
+package com.udaykdungarwal.mygcm2;
+
+/**
+ * Created by uday on 4/22/16.
+ */
+public interface ApplicationConstants {
+
+    // Php Application URL to store Reg ID created
+    static final String APP_SERVER_URL = "http://udaykdungarwal.com/gcm/gcm.php?shareRegId=true";
+
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "228211227535";
+    // Message Key
+    static final String MSG_KEY = "m";
+
+}
---
+++ app/src/main/java/com/udaykdungarwal/mygcm2/HomeActivity.java
@@ -0,0 +1,88 @@
+package com.udaykdungarwal.mygcm2;
+
+/**
+ * Created by uday on 4/22/16.
+ */
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeActivity extends Activity {
+    TextView msgET, usertitleET;
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Intent Message sent from Broadcast Receiver
+        String str = getIntent().getStringExtra("msg");
+
+        // Get Email ID from Shared preferences
+        SharedPreferences prefs = getSharedPreferences("UserDetails",
+                Context.MODE_PRIVATE);
+        String eMailId = prefs.getString("eMailId", "");
+        // Set Title
+        usertitleET = (TextView) findViewById(R.id.usertitle);
+
+        // Check if Google Play Service is installed in Device
+        // Play services is needed to handle GCM stuffs
+        if (!checkPlayServices()) {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "This device doesn't support Play services, App will not work normally",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        usertitleET.setText("Hello " + eMailId + " !");
+        // When Message sent from Broadcase Receiver is not empty
+        if (str != null) {
+            // Set the message
+            msgET = (TextView) findViewById(R.id.message);
+            msgET.setText(str);
+        }
+    }
+
+    // Check if Google Playservices is installed in Device or not
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        // When Play services not found in device
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                // Show Error dialog to install Play services
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "This device doesn't support Play services, App will not work normally",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "This device supports Play services, App will work normally",
+                    Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
+    // When Application is resumed, check for Play services support to make sure
+    // app will be running normally
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+}
