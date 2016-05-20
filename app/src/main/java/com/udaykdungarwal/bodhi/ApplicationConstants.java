23a0fb4dc39b068c45042490838b283f86bea8b1
Uday Dungarwal <uday@uday-macbook-pro.local>
2016-05-19 23:05:41 -0700

    Removing few bugs

--- app/src/main/java/com/udaykdungarwal/bodhi/ApplicationConstants.java
+++ app/src/main/java/com/udaykdungarwal/bodhi/ApplicationConstants.java
@@ -10,6 +10,7 @@ public interface ApplicationConstants {
     String APP_UPDATE_CITY_URL = "http://udaykdungarwal.com/Bodhi/update_city.php";
     String APP_INSERT_PLANT_URL = "http://udaykdungarwal.com/Bodhi/insert_plant.php";
     String APP_DELETE_PLANT_URL = "http://udaykdungarwal.com/Bodhi/delete_plant.php";
+    String APP_WATER_PLANT_URL = "http://udaykdungarwal.com/Bodhi/call_mqtt.php";
     // Google Project Number
     String GOOGLE_PROJ_ID = "228211227535";
     // Message Key
--- app/src/main/java/com/udaykdungarwal/bodhi/HomeActivity.java
+++ app/src/main/java/com/udaykdungarwal/bodhi/HomeActivity.java
@@ -5,9 +5,12 @@ package com.udaykdungarwal.bodhi;
  */
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -149,6 +152,58 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // Store Activity in Log SharedPref
+                        AsyncHttpClient client = new AsyncHttpClient();
+                        final Context applicationContext = getApplicationContext();
+                        final ProgressDialog prgDialog = new ProgressDialog(applicationContext);
+                        client.get(ApplicationConstants.APP_WATER_PLANT_URL,
+                                new AsyncHttpResponseHandler() {
+                                    // When the response returned by REST has Http
+                                    // response code '200'
+                                    @Override
+                                    public void onSuccess(String response) {
+                                        // Hide Progress Dialog
+                                        prgDialog.hide();
+                                        if (prgDialog != null) {
+                                            prgDialog.dismiss();
+                                        }
+                                        Toast.makeText(applicationContext,
+                                                "Watered the plant successfully",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+
+                                    // When the response returned by REST has Http
+                                    // response code other than '200' such as '404',
+                                    // '500' or '403' etc
+                                    @Override
+                                    public void onFailure(int statusCode, Throwable error,
+                                                          String content) {
+                                        // Hide Progress Dialog
+                                        prgDialog.hide();
+                                        if (prgDialog != null) {
+                                            prgDialog.dismiss();
+                                        }
+                                        // When Http response code is '404'
+                                        if (statusCode == 404) {
+                                            Toast.makeText(applicationContext,
+                                                    "Requested resource not found",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                                        // When Http response code is '500'
+                                        else if (statusCode == 500) {
+                                            Toast.makeText(applicationContext,
+                                                    "Something went wrong at server end",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                                        // When Http response code other than 404, 500
+                                        else {
+                                            Toast.makeText(
+                                                    applicationContext,
+                                                    "Unexpected Error occcured! [Most common Error: Device might "
+                                                            + "not be connected to Internet or remote server is not up and running], check for other errors as well",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
                         Calendar c = Calendar.getInstance();
                         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                         String formattedDate = df.format(c.getTime());
